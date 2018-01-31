3d5330ae72177a5dc925ecf2bf46e9358663fb4c
Your Name <you@example.com>
2018-01-30 16:12:14 -0800

    minor changes

--- proj1/modules/mod_calc/mod_calc.c
+++ proj1/modules/mod_calc/mod_calc.c
@@ -1,14 +1,6 @@
 #include <linux/init.h>
 #include <asm/syscall.h>
 #include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/errno.h>
-#include <linux/types.h>
-#include <linux/unistd.h>
-#include <asm/page.h>
-#include <asm/current.h>
-#include <linux/sched.h>
-#include <linux/kallsyms.h>
 
 MODULE_LICENSE("GPL"); 
 MODULE_AUTHOR("team09");    
