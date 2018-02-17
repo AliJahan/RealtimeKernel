7226652bfd882fe87e149ea85e9847e6e2ff0479
Ali Jahan <ajaha004@udr.edu>
2018-02-17 12:26:11 -0800

    system calls templates are added

---
+++ proj2/kernel/sys_cancel_rsv/sys_cancel_rsv.c
@@ -0,0 +1,25 @@
+#include <linux/export.h>
+#include <linux/errno.h>
+#include <linux/sched.h>
+#include <linux/mm.h>
+#include <linux/sem.h>
+#include <linux/msg.h>
+#include <linux/shm.h>
+#include <linux/stat.h>
+#include <linux/syscalls.h>
+#include <asm/syscall.h>
+#include <linux/mman.h>
+#include <linux/fs.h>
+#include <linux/file.h>
+#include <linux/ipc.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+/*
+ * system call for a canceling reserve
+ * <TEAM09/>
+ */
+
+SYSCALL_DEFINE1(cancel_rsv, pid_t, pid){
+
+	return -1;
+}
---
+++ proj2/kernel/sys_set_rsv/sys_set_rsv.c
@@ -0,0 +1,25 @@
+#include <linux/export.h>
+#include <linux/errno.h>
+#include <linux/sched.h>
+#include <linux/mm.h>
+#include <linux/sem.h>
+#include <linux/msg.h>
+#include <linux/shm.h>
+#include <linux/stat.h>
+#include <linux/syscalls.h>
+#include <asm/syscall.h>
+#include <linux/mman.h>
+#include <linux/fs.h>
+#include <linux/file.h>
+#include <linux/ipc.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+/*
+ * system call for setting reserver
+ * <TEAM09/>
+ */
+
+SYSCALL_DEFINE3(set_rsv, pid_t, pid, struct timespec* , C, struct timespec*, T){
+
+	return -1;
+}
